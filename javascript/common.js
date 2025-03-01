1eaca7f6a353d2a75471bc1124d63121a063ae36
Ayatullah <ayatullah284@gmail.com>
2025-03-01 16:11:09 +0600

    motamoti ses, r ekto korley poratai ses hoye jabo

---
+++ javascript/common.js
@@ -0,0 +1,10 @@
+
+function Alert(){
+    alert("board updated Successfully.")
+}
+document.getElementById("clear-btn").addEventListener("click", function (){
+    const commentContainer = document.getElementById
+    ("comment-container")
+    commentContainer.innerText = ''
+ 
+})
---
+++ javascript/js.js
@@ -0,0 +1,218 @@
+// btn-1
+document.getElementById("btn-1").addEventListener("click", function(event){
+    event.preventDefault()
+    Alert()
+
+    // Task-btn a man komailam 
+    const convertTaskNum = parseInt(document.getElementById("Task-num").innerText)
+    document.getElementById("Task-num").innerText= convertTaskNum - 1
+
+    // Nav-num a mun barailam 
+    const convertNavNum = parseInt(document.getElementById("nav-num").innerText)
+    document.getElementById("nav-num").innerText= convertNavNum + 1
+
+    // add comment 
+    const commentContainer = document.getElementById
+    ("comment-container")
+    const newComment = document.createElement('p')
+    newComment.style.padding='20px'
+    newComment.style.paddingBottom='5px'
+    newComment.style.paddingTop='10px'
+
+    // time 
+    const now = new Date();
+    const locleTime = now.toLocaleTimeString()
+
+    newComment.innerHTML = `you have completed the task Fix Mobile Button Issue at  ${ locleTime}`;
+    commentContainer.appendChild(newComment)
+
+    // btn disable
+    document.getElementById("btn-1").disabled = true
+    document.getElementById("btn-1").style= 'opacity: 0.2'
+    document.getElementById("btn-1").style.cursor = "default"
+
+})
+// btn-2
+document.getElementById("btn-2").addEventListener("click", function(event){
+    event.preventDefault()
+    Alert()
+
+    // Task-btn a man komailam 
+    const convertTaskNum = parseInt(document.getElementById("Task-num").innerText)
+    document.getElementById("Task-num").innerText= convertTaskNum - 1
+
+    // Nav-num a mun barailam 
+    const convertNavNum = parseInt(document.getElementById("nav-num").innerText)
+    document.getElementById("nav-num").innerText= convertNavNum + 1
+
+    // add comment 
+    const commentContainer = document.getElementById
+    ("comment-container")
+    const newComment = document.createElement('p')
+    newComment.style.padding='20px'
+    newComment.style.paddingBottom='5px'
+    newComment.style.paddingTop='10px'
+
+    // time 
+    const now = new Date();
+    const locleTime = now.toLocaleTimeString()
+
+    newComment.innerHTML = `you have completed the task Fix Mobile Button Issue at  ${ locleTime}`;
+    commentContainer.appendChild(newComment)
+
+    // btn disable
+    document.getElementById("btn-2").disabled = true;
+    document.getElementById("btn-2").style= 'opacity: 0.2';
+    document.getElementById("btn-2").style.cursor = "default"
+})
+// btn-3
+document.getElementById("btn-3").addEventListener("click", function(event){
+    event.preventDefault()
+    Alert()
+
+    // Task-btn a man komailam 
+    const convertTaskNum = parseInt(document.getElementById("Task-num").innerText)
+    document.getElementById("Task-num").innerText= convertTaskNum - 1
+
+    // Nav-num a mun barailam 
+    const convertNavNum = parseInt(document.getElementById("nav-num").innerText)
+    document.getElementById("nav-num").innerText= convertNavNum + 1
+
+    // add comment 
+    const commentContainer = document.getElementById
+    ("comment-container")
+    const newComment = document.createElement('p')
+    newComment.style.padding='20px'
+    newComment.style.paddingBottom='5px'
+    newComment.style.paddingTop='10px'
+
+    // time 
+    const now = new Date();
+    const locleTime = now.toLocaleTimeString()
+
+    newComment.innerHTML = `you have completed the task Fix Mobile Button Issue at  ${ locleTime}`;
+    commentContainer.appendChild(newComment)
+
+    // btn disable
+    document.getElementById("btn-3").disabled = true;
+    document.getElementById("btn-3").style= 'opacity: 0.2';
+    document.getElementById("btn-3").style.cursor = "default"
+
+})
+// btn-4
+document.getElementById("btn-4").addEventListener("click", function(event){
+    event.preventDefault()
+    Alert()
+
+    // Task-btn a man komailam 
+    const convertTaskNum = parseInt(document.getElementById("Task-num").innerText)
+    document.getElementById("Task-num").innerText= convertTaskNum - 1
+
+    // Nav-num a mun barailam 
+    const convertNavNum = parseInt(document.getElementById("nav-num").innerText)
+    document.getElementById("nav-num").innerText= convertNavNum + 1
+
+    // add comment 
+    const commentContainer = document.getElementById
+    ("comment-container")
+    const newComment = document.createElement('p')
+    newComment.style.padding='20px'
+    newComment.style.paddingBottom='5px'
+    newComment.style.paddingTop='10px'
+
+    // time 
+    const now = new Date();
+    const locleTime = now.toLocaleTimeString()
+
+    newComment.innerHTML = `you have completed the task Fix Mobile Button Issue at  ${ locleTime}`;
+    commentContainer.appendChild(newComment)
+
+    // btn disable
+    document.getElementById("btn-4").disabled = true;
+    document.getElementById("btn-4").style= 'opacity: 0.2';
+    document.getElementById("btn-4").style.cursor = "default"
+})
+// btn-5
+document.getElementById("btn-5").addEventListener("click", function(event){
+    event.preventDefault()
+    Alert()
+
+    // Task-btn a man komailam 
+    const convertTaskNum = parseInt(document.getElementById("Task-num").innerText)
+    document.getElementById("Task-num").innerText= convertTaskNum - 1
+
+    // Nav-num a mun barailam 
+    const convertNavNum = parseInt(document.getElementById("nav-num").innerText)
+    document.getElementById("nav-num").innerText= convertNavNum + 1
+
+    // add comment 
+    const commentContainer = document.getElementById
+    ("comment-container")
+    const newComment = document.createElement('p')
+    newComment.style.padding='20px'
+    newComment.style.paddingBottom='5px'
+    newComment.style.paddingTop='10px'
+
+    // time 
+    const now = new Date();
+    const locleTime = now.toLocaleTimeString()
+
+    newComment.innerHTML = `you have completed the task Fix Mobile Button Issue at  ${ locleTime}`;
+    commentContainer.appendChild(newComment)
+
+    // btn disable
+    document.getElementById("btn-5").disabled = true;
+    document.getElementById("btn-5").style= 'opacity: 0.2';
+    document.getElementById("btn-5").style.cursor = "default"
+})
+// btn-6
+document.getElementById("btn-6").addEventListener("click", function(event){
+    event.preventDefault()
+    Alert()
+
+    // Task-btn a man komailam 
+    const convertTaskNum = parseInt(document.getElementById("Task-num").innerText)
+    document.getElementById("Task-num").innerText= convertTaskNum - 1
+
+    // Nav-num a mun barailam 
+    const convertNavNum = parseInt(document.getElementById("nav-num").innerText)
+    document.getElementById("nav-num").innerText= convertNavNum + 1
+
+    // add comment 
+    const commentContainer = document.getElementById
+    ("comment-container")
+    const newComment = document.createElement('p')
+    newComment.style.padding='20px'
+    newComment.style.paddingBottom='5px'
+    newComment.style.paddingTop='10px'
+
+    // time 
+    const now = new Date();
+    const locleTime = now.toLocaleTimeString()
+
+    newComment.innerHTML = `you have completed the task Fix Mobile Button Issue at  ${ locleTime}`;
+    commentContainer.appendChild(newComment)
+
+    // btn disable
+    document.getElementById("btn-6").disabled = true;
+    document.getElementById("btn-6").style= 'opacity: 0.2';
+    document.getElementById("btn-6").style.cursor = "default"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
